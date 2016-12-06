6a4e83a064a6c6f00f8e74d1f57f3fa12c82308a
Antonio Jr <acdcjunior@gmail.com>
2016-12-06 03:08:53 -0200

    ETL: mesclar funcao de leitutra ao unico local onde ela eh usada

--- ETL-JavaScript/src/ler-csv.js
+++ ETL-JavaScript/src/ler-csv.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const readFile = require("./read-file");
+const iconvlite = require('iconv-lite');
+const fs = require('fs');
 const path = require('path');
 const Diaria = require('./diaria');
 
@@ -9,7 +10,7 @@ function lerCSV(ano, mes) {
 
     let caminhoCompletoArquivoCsv = path.resolve(__dirname, '..', 'dados', `${ano}${mesString}_Diarias.csv`);
 
-    const contents = readFile(caminhoCompletoArquivoCsv, 'ISO-8859-1');
+    const contents = readFileSync_encoding(caminhoCompletoArquivoCsv, 'ISO-8859-1');
     const linhasComoString = contents.split('\r\n');
 
     const primeiraLinha = linhasComoString.shift();
@@ -22,6 +23,11 @@ function lerCSV(ano, mes) {
 
 function separarColunas(linha) {
     return linha.split('\t');
+}
+
+function readFileSync_encoding(filename, encoding) {
+    let content = fs.readFileSync(filename);
+    return iconvlite.decode(content, encoding);
 }
 
 module.exports = lerCSV;
