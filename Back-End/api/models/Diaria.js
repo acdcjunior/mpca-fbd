51975c24d0c059c3f18afc387d023fc046b07e88
Antonio Jr <acdcjunior@gmail.com>
2016-12-07 03:40:02 -0200

    back-end: incluir colunas de diarias

--- Back-End/api/models/Diaria.js
+++ Back-End/api/models/Diaria.js
@@ -1,6 +1,21 @@
 module.exports = {
-  attributes: {
 
+  attributes: {
+    codigo: 'integer',
+    documento: 'string',
+    gestao: 'string',
+    dt_diaria: 'date',
+    valor: 'number',
+    favorecido: 'integer',
+    orgao_superior: 'integer',
+    orgao_subordinado: 'integer',
+    funcao: 'integer',
+    unidade_gestora: 'integer',
+    linguagem_cidada: 'integer',
+    acao: 'string',
+    subfuncao: 'integer',
+    programa: 'integer',
   }
+
 };
 
