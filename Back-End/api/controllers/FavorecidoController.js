752be216d68328cbf0971152d485e15ca1f96df1
Antonio Jr <acdcjunior@gmail.com>
2016-12-07 04:10:49 -0200

    back-end: alterar maneira como argumento eh recebido

--- Back-End/api/controllers/FavorecidoController.js
+++ Back-End/api/controllers/FavorecidoController.js
@@ -14,12 +14,12 @@ module.exports = {
   },
 
   diariasPorFavorecido: function(req, res) {
-    var valorBuscado = req.query.busca;
+    var valorBuscado = req.query.busca || '';
     Favorecido.query(`SELECT d.documento, d.dt_diaria, d.valor, f.nome, f.cpf
     FROM diaria d INNER JOIN favorecido f
     ON d.favorecido = f.codigo
     WHERE
-    f.nome LIKE '%$1%' or f.cpf LIKE '%$1%'`, [valorBuscado], function(err, results) {
+    f.nome LIKE $1 or f.cpf LIKE $1`, [`%${valorBuscado}%`], function(err, results) {
       if (err) {
         console.error("Erro na busca!");
         console.error(err);
