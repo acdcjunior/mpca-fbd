b19a262488a7ef509ef841355be9a6bf0bb1006f
Antonio Jr <acdcjunior@gmail.com>
2016-12-08 22:39:40 -0200

    consultas: incluir Diarias por Orgao

--- META/consultas.sql
+++ META/consultas.sql
@@ -5,8 +5,20 @@ FROM diaria d
 INNER JOIN join favorecido f ON d.favorecido = f.cpf
 WHERE f.nome LIKE '%${PARAMETRO}%' or f.cpf LIKE '%${PARAMETRO}%'
 
--- Valor por programa
+------------------------------------------------------------------------
+-- Diarias por Orgao (UG/Orgao/Org.Sup)
+SELECT *
+FROM diaria d
+INNER JOIN join unidade_gestora ug ON d.ug_pagadora = ug.codigo
+INNER JOIN join orgao org ON ug.orgao = org.codigo
+INNER JOIN join orgao orgsup sub ON org.orgao_sup = orgsup.codigo
+WHERE
+ g.nome LIKE '%${PARAMETRO}%'
+or org.nome LIKE '%${PARAMETRO}%'
+or orgsup.nome LIKE '%${PARAMETRO}%'
 
+------------------------------------------------------------------------
+-- Valor por programa
 SELECT p.nome, sum(d.valor)
 FROM diaria d
 INNER JOIN join acao a ON d.acao = a.codigo
@@ -14,8 +26,8 @@ INNER JOIN join programa p ON a.programa = p.codigo
 GROUP BY p.nome
 HAVING sum(d.valor) >= ${PARAMETRO}
 
+------------------------------------------------------------------------
 -- Valor por funcao
-
 SELECT f.nome, sum(d.valor)
 FROM diaria d
 INNER JOIN join acao a ON d.acao = a.codigo
@@ -24,8 +36,8 @@ INNER JOIN join funcao f ON f.codigo = sf.funcao
 GROUP BY f.nome
 HAVING sum(d.valor) >= ${PARAMETRO}
 
+------------------------------------------------------------------------
 -- Valor por funcao
-
 SELECT sf.nome, sum(d.valor)
 FROM diaria d
 INNER JOIN join acao a ON d.acao = a.codigo
