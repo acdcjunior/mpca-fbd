1079413345c49269fa96f7980c5125b2aa3f7d5a
Antonio Jr <acdcjunior@gmail.com>
2016-12-19 21:58:59 -0200

    faz params funcionar

--- DiariasRepository.php
+++ DiariasRepository.php
@@ -19,10 +19,10 @@ class DiariasRepository {
     function diariasPorFavorecido($parametro)
     {
 //        $sql = "SELECT d.documento, d.dt_diaria, d.valor, f.nome, f.cpf FROM diaria d INNER JOIN favorecido f ON d.favorecido = f.cpf WHERE f.nome LIKE '%?%' or f.cpf LIKE '%?%' LIMIT 10000";
-        $sql = "SELECT d.documento, d.dt_diaria, d.valor, f.nome, f.cpf FROM diaria d INNER JOIN favorecido f ON d.favorecido = f.cpf WHERE f.nome LIKE '%joao%' or f.cpf LIKE '%joao%' LIMIT 10000";
+        $sql = "SELECT d.documento, d.dt_diaria, d.valor, f.nome, f.cpf FROM diaria d INNER JOIN favorecido f ON d.favorecido = f.cpf WHERE f.nome LIKE '%?%' or f.cpf LIKE '%?%' LIMIT 10000";
 
         if ($stmt = $this->mysqli->prepare($sql)) {
-//            $stmt->bind_param("ss", $parametro, $parametro);
+            $stmt->bind_param("ss", $parametro, $parametro);
             $stmt->execute();
 
             $arr = array();
