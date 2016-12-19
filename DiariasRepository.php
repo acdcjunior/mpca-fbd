d06fa8fb3037dde839a34e14802ad17cf32b6e1d
Antonio Jr <acdcjunior@gmail.com>
2016-12-19 20:41:33 -0200

    yaaaa

--- DiariasRepository.php
+++ DiariasRepository.php
@@ -18,15 +18,11 @@ class DiariasRepository {
 
     function diariasPorFavorecido($parametro)
     {
-        $sql = <<<EOT
-    SELECT d.documento, d.dt_diaria, d.valor, f.nome, f.cpf
-    FROM diaria d
-    INNER JOIN favorecido f ON d.favorecido = f.cpf
-    WHERE f.nome LIKE '%?%' or f.cpf LIKE '%?%'
-    LIMIT 10000
-EOT;
+//        $sql = "SELECT d.documento, d.dt_diaria, d.valor, f.nome, f.cpf FROM diaria d INNER JOIN favorecido f ON d.favorecido = f.cpf WHERE f.nome LIKE '%?%' or f.cpf LIKE '%?%' LIMIT 10000";
+        $sql = "SELECT d.documento, d.dt_diaria, d.valor, f.nome, f.cpf FROM diaria d INNER JOIN favorecido f ON d.favorecido = f.cpf WHERE f.nome LIKE '%joao%' or f.cpf LIKE '%joao%' LIMIT 10000";
+
         if ($stmt = $this->mysqli->prepare($sql)) {
-            $stmt->bind_param("ss", $parametro, $parametro);
+//            $stmt->bind_param("ss", $parametro, $parametro);
             $stmt->execute();
 
             $arr = array();
@@ -98,3 +94,4 @@ EOT;
     `
 
 }
+*/
